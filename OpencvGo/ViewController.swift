f39c0953651373c32f2876f96bc886fe42cf0d61
oldtree <treeui.old@gmail.com | 435594427@qq.com>
2015-01-16 23:07:41 +0800

    Initial Commit

---
+++ OpencvGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpencvGo
+//
+//  Created by Grapestree on 15/1/16.
+//  Copyright (c) 2015年 Grapestree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
